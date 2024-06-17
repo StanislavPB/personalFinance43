7c6825ba93fe4d1cef1ecdd1e5d5f14b30c7b7af
alisaTonga <tongalise@gmail.com>
2024-06-17 18:22:14 +0200

    add Transaction

--- src/main/java/org/Backend/Entity/Transaction.java
+++ src/main/java/org/Backend/Entity/Transaction.java
@@ -1,5 +1,59 @@
 package org.Backend.Entity;
 
-public class Transaction {
+import java.time.LocalDate;
+import java.util.List;
 
+public class Transaction {
+    private TransactionType type;
+    private Long transactionNumber;
+    private LocalDate date;
+    private Double amount;
+    private Long accountID;
+    private List<Category> categories;
+    private String comment;
+
+    public Transaction(TransactionType type, Double amount, List<Category> categories, String comment) {
+        this.type = type;
+        this.amount = amount;
+        this.categories = categories;
+        this.comment = comment;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public Long getTransactionNumber() {
+        return transactionNumber;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public Long getAccountID() {
+        return accountID;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "type=" + type +
+                ", date=" + date +
+                ", amount=" + amount +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
 }
