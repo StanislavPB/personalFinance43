e898f0f900c1f34bcb73b85820c6db138c323443
VladVel24 <vlad.velykyy@gmail.com>
2024-06-18 12:06:34 +0200

    add Repository

---
+++ src/main/java/org/Backend/Repository/TransactionRepository.java
@@ -0,0 +1,58 @@
+package org.Backend.Repository;
+
+import org.Backend.Entity.Account;
+import org.Backend.Entity.Category;
+import org.Backend.Entity.Transaction;
+import org.Backend.Entity.TransactionType;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TransactionRepository implements TransactionRepositoryInterface {
+
+    private Map<Long, Transaction> transactions = new HashMap<>();
+    private Map<Long, Account> accounts = new HashMap<>();
+    private Long transactionCounter = 0L;
+
+
+    @Override
+    public void addTransaction(Transaction transaction) {
+        transactions.put(transaction.getTransactionNumber(),transaction);
+
+    }
+
+    @Override
+    public void deleteTransaction(Long transactionNumber) {
+        transactions.remove(transactionNumber);
+
+    }
+
+    @Override
+    public Transaction findById(Long transactionNumber) {
+        return transactions.get(transactionNumber);
+    }
+
+    @Override
+    public List<Transaction> findByDate(LocalDate date) {
+        return transactions.values().stream()
+                .filter(transaction -> transaction.getDate().equals(date))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Transaction> findByType(TransactionType type) {
+        return transactions.values().stream()
+                .filter(transaction -> transaction.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Transaction> findByCategory(Category category) {
+        return transactions.values().stream()
+                .filter(transaction -> transaction.getCategories().equals(category))
+                .collect(Collectors.toList());
+    }
+}
